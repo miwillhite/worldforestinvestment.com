39cb8859d8105ecc9b344c449b81cebca0825f04
Matt Willhite <matthew.willhite@gmail.com>
2016-02-16 10:47:13 -0500

    [WIP] Designing property helpers

---
+++ helpers/property_helpers.rb
@@ -0,0 +1,30 @@
+module PropertyHelpers
+
+  def slideshow
+    images = data.properties.apiary.images
+
+    content_tag(:div, class: 'ss-slides') do
+      images.map do |img|
+
+        opts = { class: 'ss-slide' }
+        opts[:class] += ' hide' unless img == images.first
+
+        path = current_path.split('/')[0..-2]
+
+        image_tag(File.join(path, 'images', img), opts)
+
+      end.join('')
+    end
+  end
+
+  def document_list
+    documents = data.properties.apiary.documents
+
+    documents.map do |doc|
+      content_tag(:li) do |li|
+        link_to(doc.title, doc.href)
+      end
+    end.join('')
+  end
+
+end
